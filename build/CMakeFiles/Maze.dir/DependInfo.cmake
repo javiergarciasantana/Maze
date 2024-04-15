733e4f25eb5d202b4b716575753e41c2092574f0
javiergarciasantana <eparis@atlassian.comls>
2024-04-15 23:46:20 +0300

    Final commit

---
+++ build/CMakeFiles/Maze.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/javiersantana/VU_Files/Artificial Intelligence/Maze/src/bfs.cc" "CMakeFiles/Maze.dir/src/bfs.cc.o" "gcc" "CMakeFiles/Maze.dir/src/bfs.cc.o.d"
+  "/Users/javiersantana/VU_Files/Artificial Intelligence/Maze/src/dfs.cc" "CMakeFiles/Maze.dir/src/dfs.cc.o" "gcc" "CMakeFiles/Maze.dir/src/dfs.cc.o.d"
+  "/Users/javiersantana/VU_Files/Artificial Intelligence/Maze/src/maze_main.cc" "CMakeFiles/Maze.dir/src/maze_main.cc.o" "gcc" "CMakeFiles/Maze.dir/src/maze_main.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Maze.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Maze.dir/src/bfs.cc.o"
+  "CMakeFiles/Maze.dir/src/bfs.cc.o.d"
+  "CMakeFiles/Maze.dir/src/dfs.cc.o"
+  "CMakeFiles/Maze.dir/src/dfs.cc.o.d"
+  "CMakeFiles/Maze.dir/src/maze_main.cc.o"
+  "CMakeFiles/Maze.dir/src/maze_main.cc.o.d"
+  "Maze"
+  "Maze.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Maze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
